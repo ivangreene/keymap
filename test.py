81be687ff398041d5483cedd345b6595b5aefee2
Ivan Greene <ivan@ivan.sh>
2018-04-15 21:06:52 -0500

    test.py: mostly covered

--- test.py
+++ test.py
@@ -32,5 +32,40 @@ class TestKeymap(unittest.TestCase):
     listitem = keymap.format_list_item(item)
     self.assertEqual(listitem, '{"HIDKeyboardModifierMappingSrc":0x700000004,"HIDKeyboardModifierMappingDst":0x700000005}')
 
+  def test_format_mappings_for_setting(self):
+    expected = '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000004,"HIDKeyboardModifierMappingDst":0x700000005},{"HIDKeyboardModifierMappingSrc":0x700000005,"HIDKeyboardModifierMappingDst":0x700000004}]}'
+    akey = keymap.get_key_code('a')
+    bkey = keymap.get_key_code('b')
+    self.assertEqual(keymap.format_mappings_for_setting([
+      keymap.key_mapping(akey, bkey), keymap.key_mapping(bkey, akey)]),
+      expected)
+
+  def test_parse_mappings(self):
+    sys_output = """
+(
+        {
+        HIDKeyboardModifierMappingDst = 30064771129;
+        HIDKeyboardModifierMappingSrc = 30064771113;
+    },
+        {
+        HIDKeyboardModifierMappingDst = 30064771113;
+        HIDKeyboardModifierMappingSrc = 30064771129;
+    }
+)
+"""
+    esc = keymap.get_key_code('escape')
+    caps = keymap.get_key_code('caps')
+    self.assertEqual(keymap.parse_mappings(sys_output),
+        [keymap.key_mapping(esc, caps), keymap.key_mapping(caps, esc)])
+
+  def test_remove_keys(self):
+    esc = keymap.get_key_code('escape')
+    akey = keymap.get_key_code('a')
+    tab = keymap.get_key_code('tab')
+    mappings = [keymap.key_mapping(esc, akey), keymap.key_mapping(akey, tab),
+        keymap.key_mapping(tab, esc)]
+    self.assertEqual(keymap.remove_keys(mappings, [akey, tab]),
+        [keymap.key_mapping(esc, akey)])
+
 if __name__ == '__main__':
   unittest.main()
